8587812ccc5420061b590beeda0960e02b1beb88
Bamuh <134942091+Bamuh@users.noreply.github.com>
2023-06-03 15:40:05 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "ami" {
    type        = string
    description = "EC2 instance"
-   default     = "ami-0f5ee92e2d63afc18"
+   default     = "ami-0e23c576dacf2e3df"
 }
 
 variable "instance_type" {
